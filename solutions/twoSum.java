89a877f0356d15307d9f780056ff2fb996cd538e
Yitong Wu <yitongw2@uci.edu>
2016-11-21 01:27:44 -0800

    Update and rename 2Sum.java to twoSum.java

---
+++ solutions/twoSum.java
@@ -0,0 +1,35 @@
+import java.util.Hashtable;
+
+public Class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public Hashtable<Integer, Integer> buildHashtable(int[] nums)
+	{
+		Hashtable<Integer, Integer> dict=new Hashtable<Integer, Integer>();
+		for (int i=0;i<nums.length;++i)
+		{
+			dict.put(nums[i], i);
+		}
+   		return dict;
+	}
+  
+ 	 public int[] twoSum(int[] nums)
+       	 {
+    		int result=new int[2];
+    		Hashtable<Integer, Integer> dict=buildHashtable(nums);
+    		for (int i=0;i<nums.length;++i)
+    		{
+      			if (dict.containsKey(target-nums[i]) && (temp=dict.get(target-nums[i]))!=i)
+      			{
+        			result[0]=i;
+        			result[1]=temp;
+        			return result;
+      			}
+    		}
+    		return result;
+	 }
+}
