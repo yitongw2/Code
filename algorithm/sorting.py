807208625dea05c7108ee371ee5443cabbd094f9
Yitong Wu <yitongw2@uci.edu>
2016-11-24 00:44:56 -0800

    Update sorting.py

--- algorithm/sorting.py
+++ algorithm/sorting.py
@@ -2,6 +2,13 @@
 import heapq
 
 def insertionSort(lis):
+	"""
+	insertion sort is one type of priority-queue sort.
+	Here, priority queue is implemented as a sorted array.
+	Therefore, insertion takes O(n) time and removeMin() takes O(1) time.
+	Imagine the priority queue starts from the left end of the array and is initially empty.
+	Each time we expand the priority queue rightward, it inserts an element to the priority queue.
+	"""
 	for i in range(len(lis)):
 		k=i
 		for j in reversed(range(i)):
@@ -13,6 +20,15 @@ def insertionSort(lis):
 
 				
 def selectionSort(lis):
+	"""
+	selection sort is still a priority queue sort.
+	Here, priority queue is implemented as an unsorted array.
+	Therefore, insertion takes O(1) time and removeMin() takes O(n) time.
+	Imagine we separate the array into sorted area on the left end and unsorted area on the right end.
+	Gradually, expand the sorted area by keep inserting the smallest element in the unsorted area to the sorted area
+	and remove it from the unsorted area untill the unsorted area is empty.
+	
+	"""
 	for x in reversed(range(len(lis))):
 		ma=x
 		for y in range(x):
@@ -22,15 +38,20 @@ def selectionSort(lis):
 		lis[ma]=lis[x]
 		lis[x]=temp
 
+def heapSort(lis):
+	"""
+	heap sort is a type of priority queue sort where we implement priority queue as heap.
+	heap is essentially a complete binary tree where the parent is smaller than any of its children.
+	heapify is a fast way to construct a heap taking only O(2*n) time (bottom-up construction).
+	By repeatedly poping the smallest element in the heap, we obtain a sorted array.
+	"""
+	heapq.heapify(lis)
+	return [heapq.heappop(lis) for x in range(len(lis))]
+
 		
 def treeSort(lis):
 	"not in-place"
 	return lis
-
-def heapSort(lis):
-	"not in-place"
-	heapq.heapify(lis)
-	return [heapq.heappop(lis) for x in range(len(lis))]
 
 
 def quickSort(lis,low,high):
