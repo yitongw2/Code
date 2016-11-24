55b258b5c4ba3e62bf340b71f6b8f5a7aedcaa88
Yitong Wu <yitongw2@uci.edu>
2016-11-24 02:32:41 -0800

    basic operations

--- data_structure/stack.py
+++ data_structure/stack.py
@@ -1,4 +1,20 @@
 class Stack:
-	def __init__(self, arr):
-		self.array=arr
-	def 
+	def __init__(self):
+		self.arr=[]
+	def top(self):
+		return self.arr[self.size()-1]
+	def pop(self):
+		return self.arr.pop();
+	def push(self, element):
+		return self.arr.append(element)
+	def size(self):
+		return len(self.arr)
+
+
+if __name__=="__main__":
+	l=[1,4,2,35,56,21,2,7]
+	s=Stack()
+	for x in l:
+		s.push(x)
+	for x in l:
+		print (s.pop())
