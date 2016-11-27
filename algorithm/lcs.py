0f846177fa2f71698e2d6a42c6ce25582577e0a4
Yitong Wu <yitongw2@uci.edu>
2016-11-27 14:48:52 -0800

    add traceBack

--- algorithm/lcs.py
+++ algorithm/lcs.py
@@ -9,11 +9,11 @@ def lcs_recur(X, Y, i, j):
 	if i==0 or j==0:
 		return ""
 	elif X[i-1]==Y[j-1]:
-		return lcs_recur(X, Y, i-1, j-1)+X[i-1]
+		return X[i-1]+lcs_recur(X, Y, i-1, j-1)
 	else:
 		stringX=lcs_recur(X, Y, i-1, j)
 		stringY=lcs_recur(X, Y, i, j-1)
-		if len(stringX)>len(stringY):
+		if len(stringX)>=len(stringY):
 			return stringX
 		else:
 			return stringY
@@ -21,6 +21,11 @@ def lcs_recur(X, Y, i, j):
 def lcs(X,Y):
 	"""
 	dynamic programming 
+	use memoization --> build a 2D list for storage
+	recurrence formula: 
+		if x==0 or y==0: set table cell at (x, y) to 0 // one of the string has 0 length, no common char
+		elif X[x-1]==Y[y-1]: increment table cell at (x-1, y-1) with 1
+		else: choose the max value between table cell (x-1, y) and (x, y-1)
 	"""
 	table=[]
 	for x in range(len(X)+1):
@@ -34,13 +39,37 @@ def lcs(X,Y):
 			else:
 				table[x][y]=max(table[x-1][y], table[x][y-1])
 	return table	
-	
+
+def traceBack(X, Y, table):
+	"""
+	Given a dynamic programming table, trace back the actual common substring of 
+	X and Y.
+	"""
+	i=len(X)
+	j=len(Y)
+	result=[]
+	while (i+j>0):
+		if X[i-1]==Y[j-1]:
+			result.append(X[i-1])
+			i-=1
+			j-=1
+		else:
+			if table[i-1][j]>table[i][j-1]:
+				i-=1
+			else:
+				j-=1
+	return "".join(reversed(result))	
+		
 
 if __name__=="__main__":
 	X="dynamic"
 	Y="programming"
-	print(lcs_recur(X, Y, len(X), len(Y)))
-	for x in lcs(X,Y):
+	print ("X=", X, "Y=", Y)
+	#print("LCS(recursive)=", lcs_recur(X, Y, len(X), len(Y)))
+	table=lcs(X,Y)
+	print ("LCS(dynamic programming table)")
+	for x in table:
 		print (x)
-	
+	print ("result=", traceBack(X,Y,table))
+		
 
